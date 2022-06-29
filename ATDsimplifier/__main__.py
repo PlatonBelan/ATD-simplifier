98c8fa71a3cba9455e5cc8873be6ba4f10dea8ff
PlatonBelan <platonbelan@gmail.com>
2022-06-29 15:14:23 +0300

    fix: delete unused variable

--- ATDsimplifier/__main__.py
+++ ATDsimplifier/__main__.py
@@ -6,4 +6,3 @@ if __name__=="__main__":
     simplifier=Simplifier(input_file="D:/ATD-simplifier/test/test_data.shp")
     boundary_10=simplifier.simplify(10)
     boundary_10=simplifier.simplify(30)
-    RUN_FROM_CONSOLE=True
