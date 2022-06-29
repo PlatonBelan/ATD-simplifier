71c400382ffe4faac535ae9a56c5dbec0dacf895
PlatonBelan <platonbelan@gmail.com>
2022-06-29 19:03:16 +0300

    update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import ATDsimplifier
+from ATDsimplifier.adapter_mapshaper_simplifier import Adapter_simplifier2mapshaper
+from ATDsimplifier.mapshaper_simplifier import Mapshaper_simplifier
 
-simplifier=ATDsimplifier.simplifier.Simplifier(input_file="D:/ATD-simplifier/test/test_data.shp")
+simplifier=Adapter_simplifier2mapshaper(Mapshaper_simplifier(input_file="D:/ATD-simplifier/test/test_data.shp"))
 boundary_10=simplifier.simplify(10)
 boundary_10=simplifier.simplify(30)
