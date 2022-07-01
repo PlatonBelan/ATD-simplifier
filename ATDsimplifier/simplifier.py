cc4cc4bbba98a815776edef42265e226cb78ea67
PlatonBelan <platonbelan@gmail.com>
2022-07-01 18:09:45 +0300

    feat: implementation of the interface ISimplifier  using adapters

---
+++ ATDsimplifier/simplifier.py
@@ -0,0 +1,36 @@
+from os import path
+from ATDsimplifier.logger import log
+from ATDsimplifier.interface import ISimplifier
+from adapters.adapter_mapshaper_simplifier import AdapterMapshaper2Simplifier
+
+class Simplifier(ISimplifier):
+
+    adapter=None
+
+
+    def __init__(self, input_file, adapter: ISimplifier):
+        self.adapter=adapter
+        self.input_path = self.get_path(input_file)
+        self.check()
+
+    def get_path(self, input_file):
+        return path.relpath(input_file, start="../adapters")
+
+    def check(self):
+        self.adapter.check()
+
+    def simplify(self, percentage):
+        if type(percentage) == int:
+            percentage = str(percentage)+'%'
+        try:
+            log.debug("Start simplify "+self.input_path)
+            output = "./data/result_"+percentage+".shp"
+
+            self.adapter.simplify(self.input_path, percentage, output)
+
+            log.debug("Simplification is finished. " +
+                  percentage + " vertices left")
+            log.debug("Output file: " + self.input_path)
+        except Exception:
+            log.error("Error: There are problems with the adapter or parametrs.\
+                \nCheck the data types or contact the developer")
