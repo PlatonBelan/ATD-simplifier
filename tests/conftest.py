b7764defac26de1c8f47808e3f1b451f7c929a87
PlatonBelan <platonbelan@gmail.com>
2022-07-07 17:17:12 +0300

    feat: add tests for adapter

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,12 @@ from adapters.adapter_mapshaper_simplifier import AdapterMapshaper2Simplifier
 
 import pytest
 
+
 @pytest.fixture(scope="module")
 def simplifier():
     return Simplifier("./test/test_data.shp", AdapterMapshaper2Simplifier())
+
+
+@pytest.fixture(scope="module")
+def adapter():
+    return AdapterMapshaper2Simplifier()
