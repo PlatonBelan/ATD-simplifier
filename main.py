c08e58fddd2fb6f5263afb592acd765544dbb083
PlatonBelan <platonbelan@gmail.com>
2022-07-17 19:48:39 +0300

    add comments to main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from ATDsimplifier.simplifier import Simplifier
 from ATDsimplifier import choose_adaper
 
-percentage=10
-adapter=choose_adaper(percentage)
+percentage = 10
+adapter = choose_adaper(percentage)  # choose the most suitable adapter
+# for a given percentage of remaining vertices
 simplifier = Simplifier(
     input_file="./test_data/test_data.shp", adapter=adapter)
-boundary_simplified = simplifier.simplify(percentage)
+simplifier.simplify(percentage)
+# output file (*.shp) will be written to ./results/
